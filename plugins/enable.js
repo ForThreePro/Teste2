6548066a31fff0b16da4ba3d1a90e3d16401f3fb
YALLICO OWNER <yallico2024@gmail.com>
2026-09-11 18:36:38 -0500

    Actualizar enable.js

--- plugins/enable.js
+++ plugins/enable.js
@@ -1,4 +1,8 @@
+import moment from 'moment-timezone'
+moment.locale('es')
+
 let handler = async (m, { conn, command, args, isOwner, isAdmin, isROwner }) => {
+  const fecha = moment.tz('America/Lima').format('DD/MM/YYYY hh:mm:ss a')
   let isEnable = /true|enable|(turn)?on|1/i.test(args[0])
   let chat = global.db.data.chats[m.chat]
   let bot = global.db.data.settings[conn.user.jid] || {}
@@ -10,18 +14,23 @@ let handler = async (m, { conn, command, args, isOwner, isAdmin, isROwner }) => 
 
   if (!args[0]) {
     await react('❌')
-    let error = `𐔌 ꒱ ***GARFIEL BOT*** 𐔌 ꒱ ⚠️
+    let error = `🍕 𓆩 𝗚𝗔𝗥𝗙𝗜𝗘𝗟𝗗 𝗕𝗢𝗧 𓆪 🍕
 
-.⃟𖥔 ݁. 𖦹˙— \`\`CONFIGURACIÓN\`\` —˙𖦹.⚙️꒷
+⤷ ┇ 𝐂𝐎𝐍𝐅𝐈𝐆𝐔𝐑𝐀𝐂𝐈𝐎𝐍 ﹒ ON/OFF ：✿ 。
+꒰ ◞⁺⊹ ．${fecha}
 
-── *📝 AVISO* ╏
+.⃟𖥔 ݁. 𖦹˙— \`\`ERROR\`\` ❌ —˙𖦹.꒷
+
+── *📝 AVISO* ╏ 🍕
 ❌ ➛ Falta activar o desactivar
 
-── *💡 USO* ╏
-➛ welcome on / off
-➛ antilink on / off
-➛ nsfw on / off
+── *💡 USO* ╏ 🍕
+➛.welcome on / off
+➛.antilink on / off
+➛.nsfw on / off
 
+━━━━━━━━━━━
+🍕 *GARFIELD BOT* 🍕
 ━━━━━━━━━━━`
     return conn.sendMessage(m.chat, { text: error }, { quoted: m })
   }
@@ -29,71 +38,88 @@ let handler = async (m, { conn, command, args, isOwner, isAdmin, isROwner }) => 
   let fail = false
   switch (type) {
     case 'welcome': case 'bienvenida':
-      if (m.isGroup &&!isAdmin) { await react('🔒'); return conn.sendMessage(m.chat, { text: `𐔌 ꒱ ***GARFIEL BOT*** 𐔌 ꒱ 🔒\n\n── *📝 AVISO* ╏\n🔒 ➛ Solo admins\n━━━━━━━━━━━` }, { quoted: m }); fail = true; break }
+      if (m.isGroup &&!isAdmin) { await react('🔒'); fail = true; break }
       chat.bienvenida = isEnable
       break
     case 'subbots': case 'serbot':
-      if (!isROwner) { await react('🔒'); return conn.sendMessage(m.chat, { text: `𐔌 ꒱ ***GARFIEL BOT*** 𐔌 ꒱ 🔒\n\n── *📝 AVISO* ╏\n🔒 ➛ Solo Owner\n━━━━━━━━━━━` }, { quoted: m }); fail = true; break }
+      if (!isROwner) { await react('🔒'); fail = true; break }
       bot.jadibotmd = isEnable
       break
     case 'antispam':
-      if (!isOwner) { await react('🔒'); return conn.sendMessage(m.chat, { text: `𐔌 ꒱ ***GARFIEL BOT*** 𐔌 ꒱ 🔒\n\n── *📝 AVISO* ╏\n🔒 ➛ Solo Owner\n━━━━━━━━━━━` }, { quoted: m }); fail = true; break }
+      if (!isOwner) { await react('🔒'); fail = true; break }
       bot.antiSpam = isEnable
       break
     case 'antilink':
-      if (m.isGroup &&!isAdmin) { await react('🔒'); return conn.sendMessage(m.chat, { text: `𐔌 ꒱ ***GARFIEL BOT*** 𐔌 ꒱ 🔒\n\n── *📝 AVISO* ╏\n🔒 ➛ Solo admins\n━━━━━━━━━━━` }, { quoted: m }); fail = true; break }
+      if (m.isGroup &&!isAdmin) { await react('🔒'); fail = true; break }
       chat.antiLink = isEnable
       break
     case 'antibot':
-      if (m.isGroup &&!isAdmin) { await react('🔒'); return conn.sendMessage(m.chat, { text: `𐔌 ꒱ ***GARFIEL BOT*** 𐔌 ꒱ 🔒\n\n── *📝 AVISO* ╏\n🔒 ➛ Solo admins\n━━━━━━━━━━━` }, { quoted: m }); fail = true; break }
+      if (m.isGroup &&!isAdmin) { await react('🔒'); fail = true; break }
       chat.antiBot = isEnable
       break
     case 'modoadmin':
-      if (m.isGroup &&!isAdmin) { await react('🔒'); return conn.sendMessage(m.chat, { text: `𐔌 ꒱ ***GARFIEL BOT*** 𐔌 ꒱ 🔒\n\n── *📝 AVISO* ╏\n🔒 ➛ Solo admins\n━━━━━━━━━━━` }, { quoted: m }); fail = true; break }
+      if (m.isGroup &&!isAdmin) { await react('🔒'); fail = true; break }
       chat.modoadmin = isEnable
       break
     case 'nsfw': case 'antinopor':
-      if (m.isGroup &&!isAdmin) { await react('🔒'); return conn.sendMessage(m.chat, { text: `𐔌 ꒱ ***GARFIEL BOT*** 𐔌 ꒱ 🔒\n\n── *📝 AVISO* ╏\n🔒 ➛ Solo admins\n━━━━━━━━━━━` }, { quoted: m }); fail = true; break }
+      if (m.isGroup &&!isAdmin) { await react('🔒'); fail = true; break }
       chat.nsfw = isEnable
       break
     case 'audios':
       chat.audios = isEnable
       break
     case 'autoread': case 'autoleer':
-      if (!isROwner) { await react('🔒'); return conn.sendMessage(m.chat, { text: `𐔌 ꒱ ***GARFIEL BOT*** 𐔌 ꒱ 🔒\n\n── *📝 AVISO* ╏\n🔒 ➛ Solo Owner\n━━━━━━━━━━━` }, { quoted: m }); fail = true; break }
+      if (!isROwner) { await react('🔒'); fail = true; break }
       global.opts['autoread'] = isEnable
       break
     case 'antiprivado':
-      if (!isOwner) { await react('🔒'); return conn.sendMessage(m.chat, { text: `𐔌 ꒱ ***GARFIEL BOT*** 𐔌 ꒱ 🔒\n\n── *📝 AVISO* ╏\n🔒 ➛ Solo Owner\n━━━━━━━━━━━` }, { quoted: m }); fail = true; break }
+      if (!isOwner) { await react('🔒'); fail = true; break }
       bot.antiPrivate = isEnable
       break
     default:
       return
   }
 
-  if (fail) return
-  await react(isEnable? '🟢' : '🔴')
+  if (fail) {
+    let lock = `🍕 𓆩 𝗚𝗔𝗥𝗙𝗜𝗘𝗟𝗗 𝗕𝗢𝗧 𓆪 🍕
 
-  // IMAGEN FIJA QUE ME DISTE
-  let catalogoImg = { url: 'https://files.evogb.win/QFXQtu.jpg' }
+⤷ ┇ 𝐀𝐂𝐄𝐒𝐎 𝐃𝐄𝐍𝐄𝐆𝐀𝐃𝐎 ﹒ ${type.toUpperCase()} ：✿ 。
+꒰ ◞⁺⊹ ．${fecha}
+
+.⃟𖥔 ݁. 𖦹˙— \`\`ERROR\`\` 🔒 —˙𖦹.꒷
+
+── *📝 AVISO* ╏ 🍕
+🔒 ➛ No tienes permisos para esto
+
+━━━━━━━━━━━
+🍕 *GARFIELD BOT* 🍕
+━━━━━━━━━━━`
+    return conn.sendMessage(m.chat, { text: lock }, { quoted: m })
+  }
+
+  await react(isEnable? '🟢' : '🔴')
 
   let estadoTexto = isEnable? 'Activado' : 'Desactivado'
   let estadoEmoji = isEnable? '🟢' : '🔴'
 
-  let statusTxt = `𐔌 ꒱ ***GARFIEL BOT*** 𐔌 ꒱ ✅
+  let statusTxt = `🍕 𓆩 𝗚𝗔𝗥𝗙𝗜𝗘𝗟𝗗 𝗕𝗢𝗧 𓆪 🍕
 
-.⃟𖥔 ݁. 𖦹˙— \`\`CONFIGURACIÓN\`\` —˙𖦹.⚙️꒷
+⤷ ┇ 𝐂𝐎𝐍𝐅𝐈𝐆𝐔𝐑𝐀𝐂𝐈𝐎𝐍 ﹒ ${type.toUpperCase()} ：✿ 。
+꒰ ◞⁺⊹ ．${fecha}
 
-── *📊 ESTADO* ╏
+.⃟𖥔 ݁. 𖦹˙— \`\`ACTUALIZADO\`\` ⚙️ —˙𖦹.꒷
+
+── *📊 ESTADO* ╏ 🍕
 ⚙️ ➛ Función: *${type}*
 ${estadoEmoji} ➛ Estado: *${estadoTexto}*
 👑 ➛ Por: @${m.sender.split('@')[0]}
 
+━━━━━━━━━━━
+🍕 *GARFIELD BOT* 🍕
 ━━━━━━━━━━━`
 
   await conn.sendMessage(m.chat, {
-    image: catalogoImg,
-    caption: statusTxt,
+    text: statusTxt,
     mentions: [m.sender]
   }, { quoted: m })
 }
