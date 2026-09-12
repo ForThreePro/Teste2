0d3d5273c902ce8721eb81d4532fdbe6efb40061
YALLICO OWNER <yallico2024@gmail.com>
2026-09-11 23:22:28 -0500

    Actualizar group-fantasmas.js

--- plugins/group-fantasmas.js
+++ plugins/group-fantasmas.js
@@ -1,36 +1,37 @@
-let handler = async (m, { conn, text, participants, isAdmin, isBotAdmin }) => {
-    if (!m.isGroup) return conn.reply(m.chat, `*[⚔️] Solo grupos*`, m)
-
+let handler = async (m, { conn, text, participants }) => {
     let member = participants.map(u => u.id)
-    let sum = text? parseInt(text) : member.length
-    let total = 0
-    let sider = []
-
+    if (!text) {
+        var sum = member.length
+    } else {
+        var sum = text
+    }
+    var total = 0
+    var sider = []
     for (let i = 0; i < sum; i++) {
-        let userId = member[i]
-        let userData = global.db.data.users[userId] || {}
-        let user = participants.find(u => u.id == userId)
-
-        // Fantasma = menos de 3 mensajes Y no es admin
-        if ((userData.chat || 0) < 3 &&!user.admin &&!user.isSuperAdmin) {
-            if(userData.whitelist!== true){ // Si no está en whitelist
+        let users = m.isGroup ? participants.find(u => u.id == member[i]) : {}
+        if ((typeof global.db.data.users[member[i]] == 'undefined' || global.db.data.users[member[i]].chat == 0) && !users.isAdmin && !users.isSuperAdmin) {
+            if (typeof global.db.data.users[member[i]] !== 'undefined') {
+                if (global.db.data.users[member[i]].whitelist == false) {
+                    total++
+                    sider.push(member[i])
+                }
+            } else {
                 total++
-                sider.push(userId)
+                sider.push(member[i])
             }
         }
     }
-
-    if (total == 0) return conn.reply(m.chat, `*[⚔️] En Este Grupo No Hay Fantasmas✨🍷*`, m)
+    if (total == 0) return conn.reply(m.chat, `*[⚔️] En Este Grupo No Hay Fantasmas✨🍷*`, m, rcanal)
 
     const stickerUrl = 'https://files.catbox.moe/agx2sc.webp';
     m.react('💫')
     await conn.sendFile(m.chat, stickerUrl, 'sticker.webp', '', m, null);
 
-    m.reply(`[⚠ *FANTASMAS - INACTIVOS* ⚠]\n\n𝙶𝚁𝚄𝙿𝙾: ${await conn.getName(m.chat)}\n𝙼𝙸𝙴𝙼𝙱𝚁𝙾𝚂: ${sum}\n\n[ ⇲ 𝙻𝙸𝚂𝚃𝙰 𝙳𝙴 𝙵𝙰𝙽𝚃𝙰𝚂𝙼𝙰𝚂 ⇱ ]\n${sider.map(v => ' 👻 @' + v.replace(/@.+/, '')).join('\n')}\n\n*CRITERIO:* <3 mensajes desde que se activó el bot`, null, { mentions: sider })
+    m.reply(`[⚠ *FANTASMAS - INACTIVOS* ⚠]\n\n𝙶𝚁𝚄𝙿𝙾: ${await conn.getName(m.chat)} & 𝙼𝙸𝙴𝙼𝙱𝚁𝙾𝚂: \n${sum}\n\n[ ⇲ 𝙻𝙸𝚂𝚃𝙰 𝙳𝙴 𝙵𝙰𝙽𝚃𝙰𝚂𝙼𝙰𝚂 ⇱ ]\n${sider.map(v => '  👻 @' + v.replace(/@.+/, '')).join('\n')}\n\n𝙽𝙾𝚃𝙰: 𝙴𝚂𝚃𝙾 𝙽𝙾 𝙿𝚄𝙴𝙳𝙴 𝚂𝙴𝚁 𝟷𝟶𝟶% 𝙲𝙾𝚁𝚁𝙴𝙲𝚃𝙾, 𝙴𝙻 𝙱𝙾𝚃 𝙸𝙽𝙸𝙲𝙸𝙰 𝙴𝙻 𝙲𝙾𝙽𝚃𝙴𝙾 𝙳𝙴 𝙼𝙴𝙽𝚂𝙰𝙹𝙴𝚂 𝙳𝙴𝚂𝙳𝙴 𝙻𝙰 𝙰𝙲𝚃𝙸𝚅𝙰𝙲𝙸Ó𝙽 𝙴𝙽 𝙴𝚂𝚃𝙴 𝙶𝚁𝚄𝙿𝙾`, null, { mentions: sider })
 }
-handler.help = ['fantasmas [cantidad]']
+handler.help = ['fantasmas']
 handler.tags = ['gc']
 handler.command = /^(fantasmas|sider)$/i
 handler.admin = true
-handler.botAdmin = false
+handler.botAdmin = true
 export default handler
