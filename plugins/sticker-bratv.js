21a0bb5ecb595d2a964ff60f22521125e031937b
YALLICO OWNER <yallico2024@gmail.com>
2026-09-11 19:24:19 -0500

    Actualizar sticker-bratv.js

--- plugins/sticker-bratv.js
+++ plugins/sticker-bratv.js
@@ -1,5 +1,7 @@
 import { sticker } from '../lib/sticker.js'
 import axios from 'axios'
+import moment from 'moment-timezone'
+moment.locale('es')
 
 const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms))
 
@@ -13,6 +15,7 @@ const fetchStickerVideo = async (text) => {
 }
 
 const handler = async (m, { conn, text }) => {
+  const fecha = moment.tz('America/Lima').format('DD/MM/YYYY hh:mm:ss a')
   const react = async (text) => {
     try { await conn.sendMessage(m.chat, { react: { text: text, key: m.key } }) } catch {}
   }
@@ -20,14 +23,27 @@ const handler = async (m, { conn, text }) => {
   try {
     let userId = m.sender
     let packstickers = global.db.data.users[userId] || {}
-    let texto1 = packstickers.text1 || global.packsticker
-    let texto2 = packstickers.text2 || global.packsticker2
+    let texto1 = packstickers.text1 || global.packsticker || 'GARFIELD BOT'
+    let texto2 = packstickers.text2 || global.packsticker2 || 'V2.6'
 
     text = m.quoted?.text || text
     if (!text) {
       await react('❌')
       return conn.sendMessage(m.chat, {
-        text: `𐔌 ꒱ ***BRATV*** 𐔌 ꒱ ⚠️\n\n── *📖 USO* ╏\n➛ Responde a un mensaje o escribe texto\n➛ Ejemplo:.bratv Hola\n━━━━━━━━━━━`
+        text: `🍕 𓆩 𝗚𝗔𝗥𝗙𝗜𝗘𝗟𝗗 𝗕𝗢𝗧 𓆪 🍕
+
+⤷ ┇ 𝐁𝐑𝐀𝐓𝐕 ﹒ ERROR ：✿ 。
+꒰ ◞⁺⊹ ．${fecha}
+
+.⃟𖥔 ݁. 𖦹˙— \`\`ERROR DE USO\`\` ⚠️ —˙𖦹.꒷
+
+── *📖 USO* ╏ 🍕
+➛ Responde a un mensaje o escribe texto
+➛ Ejemplo:.bratv Hola
+
+━━━━━━━━━━━
+🍕 *GARFIELD BOT* 🍕
+━━━━━━━━━━━`
       }, { quoted: m })
     }
 
@@ -38,9 +54,26 @@ const handler = async (m, { conn, text }) => {
     await react('✅')
 
   } catch (e) {
+    console.error("[BRATV ERROR]:", e)
     await react('❌')
     conn.sendMessage(m.chat, {
-      text: `𐔌 ꒱ ***BRATV*** 𐔌 ꒱ ⚠️\n\n.⃟𖥔 ݁. 𖦹˙— \`\`ERROR\`\` —˙𖦹.❌꒷\n\n── *📝 AVISO* ╏\n❌ ➛ Se produjo un problema\n💡 ➛ Usa report para informarlo\n── *📊 DETALLE* ╏\n\`\`${e.message}\`\n━━━━━━━━━━━`
+      text: `🍕 𓆩 𝗚𝗔𝗥𝗙𝗜𝗘𝗟𝗗 𝗕𝗢𝗧 𓆪 🍕
+
+⤷ ┇ 𝐁𝐑𝐀𝐓𝐕 ﹒ ERROR ：✿ 。
+꒰ ◞⁺⊹ ．${fecha}
+
+.⃟𖥔 ݁. 𖦹˙— \`\`ERROR\`\` ❌ —˙𖦹.꒷
+
+── *📝 AVISO* ╏ 🍕
+❌ ➛ Se produjo un problema
+💡 ➛ La API puede estar caída
+
+── *📊 DETALLE* ╏ 🍕
+\`\`${e.message}\`\`
+
+━━━━━━━━━━━
+🍕 *GARFIELD BOT* 🍕
+━━━━━━━━━━━`
     }, { quoted: m })
   }
 }
