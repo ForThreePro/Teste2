2044ab7ff1f03ec68bf71489b37d74a12c1b9e7f
YALLICO OWNER <yallico2024@gmail.com>
2026-09-12 14:03:32 -0500

    Actualizar _horny.js

--- plugins/_horny.js
+++ plugins/_horny.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, participants }) => {
-    let defaultImg = 'https://files.evogb.win/n4InsB.jpg'
+    let defaultImg = 'https://files.evogb.win/QFXQtu.jpg'
     let defaultBg = 'https://files.evogb.win/7BY3Yv.jpg'
     let key = 'proyectsV2'
 
